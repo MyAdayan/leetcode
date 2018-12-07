5aad932377754a65fda50b06513f0c6327685c79
沛公 <990390177@qq.com>
2018-12-07 15:42:22 +0800

    [add] 提交一个将大写字母转换为小写字母的算法

---
+++ leetcode_String/src/test/java/ToLowerCaseTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ToLowerCaseTest {
+
+    @Test
+    public void toLowerCase() {
+        ToLowerCase test_class = new ToLowerCase();
+        String string = "AbCDe";
+        System.out.println(test_class.toLowerCase(string));
+    }
+}
